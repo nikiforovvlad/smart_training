ca9ebd2c341bdcbeaac25a47af8dde8a1d9cf195
Vladimir Nikiforov <vladimir.nikiforov@gmail.com>
2018-07-03 20:06:13 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+x = 3
+y = 4
+
+
+operation = raw_input("Bitte geben Sie wie folgt ein, + - / *")
+
+if operation == "+":
+    print x + y
+elif operation == "-":
+    print x - y
+elif operation == "/":
+    print x / y
+else:
+    print x * y
